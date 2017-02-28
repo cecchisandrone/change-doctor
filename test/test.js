43862088a67d5983474d3b86bc3835afd19e83bb
Alessandro Dionisi <cecchisandrone@gmail.com>
2017-02-28 10:02:53 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -24,7 +24,7 @@ describe('angularjs homepage todo list', function () {
         browser.ignoreSynchronization = true;  // or false
         browser.get('https://www.poslazio.it/');
         element(by.css('#username')).sendKeys('cecchisandrone');
-        element(by.css('#password')).sendKeys('FK2w6kL7Wl');
+        element(by.css('#password')).sendKeys('password');
         element(by.css('#loginFormBean > p:nth-child(4) > input')).click();
         element(by.css('#boxServizi > div > ul:nth-child(4) > li:nth-child(1) > div > a')).click();
         element(by.css('#center > div > ul:nth-child(14) > li > a')).click();
